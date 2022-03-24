ba46d52ac1390076d5df5a62b6f74b4c6337f0f0
Aaron Paden <aaronbpaden@gmail.com>
2022-03-24 10:50:45 -0500

    Add homepage.

--- routes/router.js
+++ routes/router.js
@@ -5,6 +5,13 @@ const router = express.Router();
 
 router.use(express.static('public'));
 
+router.get('/', (req, res) => {
+	res.render('pages/home', {
+		title: 'Example movie db',
+		name: 'Movies DB',
+	});
+});
+
 router.get('*', (req, res) => {
 	switch(req.url) {
 		case '/favicon.ico':
